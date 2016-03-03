e81b46391c2d1ef96da86169bfd929ad9a341d7c
followcat <followcat@163.com>
2016-03-03 11:20:33 +0800

    Fix bug about screenshout and improve the compatibility.

--- screen_storage.py
+++ screen_storage.py
@@ -1,52 +1,90 @@
+import re
+import time
+import math
 import StringIO
+import xml.etree.ElementTree as ET
 
 import Image
 
 
-def webviewfullscreen(webdriver):
+def contain_size(driver):
+    driver.switch_to.context('NATIVE_APP')
+    xmlsrc = driver.page_source
+    xmlobj = ET.fromstring(xmlsrc.encode('utf-8'))[0]
+    bounds = [int(each) for each in re.findall(r'[\d]+', xmlobj.get('bounds'))]
+    size = {'width':bounds[2]-bounds[0], 'height':bounds[3]-bounds[1]}
+    return size
+
+
+def webview_locate(driver):
+    """"""
+    def area(elem):
+        bounds = [int(each) for each in re.findall(r'[\d]+', elem.get('bounds'))]
+        return (bounds[2]-bounds[0])*(bounds[3]-bounds[1])
+    def allview(xmlobj, save=None):
+        if save is None:
+            save = []
+        elems = list(xmlobj)
+        for each in elems:
+            allview(each, save)
+        if xmlobj.tag == 'android.view.View':
+            save.append(xmlobj)
+        return save
+    driver.switch_to.context('NATIVE_APP')
+    xmlsrc = driver.page_source
+    xmlobj = ET.fromstring(xmlsrc.encode('utf-8'))
+    results = sorted(allview(xmlobj), key=lambda each: area(each), reverse=True)
+    points = [int(each) for each in re.findall(r'[\d]+', results[0].get('bounds'))]
+    return {'x':points[0], 'y':points[1]}
+
+def webviewfullscreen(driver):
     """"""
     cimgs = []
     screenshots = []
-    webdriver.switch_to.context('WEBVIEW_1')
-    webdriver.execute_script('window.scrollTo(0, 0);')
-    webdriver.implicitly_wait(1000)
-    webdriver.switch_to.context('NATIVE_APP')
-    webview = webdriver.find_element_by_class_name('android.webkit.WebView')
-    size = webview.size
-    location = webview.location
+    driver.switch_to.context('WEBVIEW_1')
+    driver.execute_script('window.scrollTo(0, 0);')
+    time.sleep(1)
+    view_size = contain_size(driver)
+    webview_location = webview_locate(driver)
 
-    view = webdriver.find_element_by_class_name('android.view.View')
-    last_scoll = view.size['height']%size['height']
+    driver.switch_to.context('WEBVIEW_1')
+    total_hegiht = driver.execute_script('return document.body.scrollHeight')
+    screen_height = driver.execute_script('return window.screen.height')
 
-    webdriver.switch_to.context('WEBVIEW_1')
-    total_hegiht = webdriver.execute_script('return document.body.scrollHeight')
-    screen_height = webdriver.execute_script('return window.screen.height')
-
-    times = int(total_hegiht/screen_height)
-    for index in range(times+1):
-        index = index + 1
-        webdriver.execute_script('window.scrollTo(0, %d);' %
-            (index*screen_height-location['y']/times*index))
-        webdriver.switch_to.context('NATIVE_APP')
-        png = webdriver.get_screenshot_as_png()
+    scale = float(screen_height)/view_size['height']
+    scroll_height = math.ceil((view_size['height']-webview_location['y'])*scale)
+    moved = 0
+    count = 0
+    scrolled = 0
+    times = math.ceil(float(total_hegiht/screen_height)) + 1
+    while True:
+        last_scrolled = moved
+        driver.execute_script('window.scrollTo(0, %d);' % (count*scroll_height))
+        moved = driver.execute_script('return document.body.scrollTop') - scrolled
+        scrolled = driver.execute_script('return document.body.scrollTop')
+        time.sleep(1)
+        driver.switch_to.context('NATIVE_APP')
+        png = driver.get_screenshot_as_png()
         screenshots.append(png)
-        webdriver.switch_to.context('WEBVIEW_1')
+        driver.switch_to.context('WEBVIEW_1')
+        count += 1
+        if last_scrolled > moved or (last_scrolled > 0 and moved == 0):
+            break
+    last_scroll = int(moved/scale)
 
-    webdriver.switch_to.context('NATIVE_APP')
+    driver.switch_to.context('NATIVE_APP')
     for each in range(len(screenshots)):
         img = Image.open(StringIO.StringIO(screenshots[each]))
+        region = [webview_location['x'], webview_location['y'],
+                  view_size['width'], view_size['height']]
         if each == len(screenshots)-1:
-            region = (location['x'], view.size['height']+view.location['y']-last_scoll,
-                      size['width'], size['height'] + location['y'])
-        else:
-            region = (location['x'], location['y'],
-                      size['width'], size['height'] + location['y'])
-        cimg = img.crop(region)
+            region[1]=region[3]-last_scroll
+        cimg = img.crop(tuple(region))
         cimgs.append(cimg)
         cimg.save('/tmp/crop_%d.png' % each)
 
     image_height = sum([each.size[1] for each in cimgs])
-    result_image = Image.new('RGBA', (view.size['width'], image_height))
+    result_image = Image.new('RGBA', (view_size['width'], image_height))
     paste_height = 0
     for each in cimgs:
         result_image.paste(each, (0, paste_height))
